b588985785a75c774191871e55b74d9ab964cb0d
Benjamin Lin <blin432@gmail.com>
2019-06-05 22:45:42 -0400

    cities

--- exercises/06-cities.js
+++ exercises/06-cities.js
@@ -7,9 +7,24 @@
 // ];
 // and returns a new array containing only those cities whose temperature is
 // cooler than 70 degrees.
+function coolCities(array){
+    var newArray=array.filter(function(element){
+        if (element.temperature < 70){
+            return element;
+        }
 
+    });
+    return newArray;
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "cityNames" which takes an array of city objects like the
 // above problem and returns an array of the cities names.
+function cityNames(array){
+    var newArray=array.map(function(element){
+        return element.name;
+
+    });
+    return newArray;
+}
